f0ec89082ceb9f9cb014bc0cbe0ad159c306399a
machiner123 <igor.atakhanov@gmail.com>
2016-06-19 21:25:46 -0400

    end of chap 5

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -5,6 +5,12 @@ from lists.models import Item
 
 
 def home_page(request):
+    '''
+    check if request is POST, if yes create Item object with text == item_text, redirect
+    to homepage. whether post or not, store all created Item objects in item, render 
+    home.html, with {% items %} python object in for loop being replaced the list of
+    objects in var items
+    '''
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/')
