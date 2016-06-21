94103e8b25ddb75366569c51270c46205cf5ff66
machiner123 <igor.atakhanov@gmail.com>
2016-06-21 16:32:32 -0400

    refactored tests to just before list.html completely works

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -4,16 +4,14 @@ from lists.models import Item
 
 
 
+    
 def home_page(request):
-    '''
-    check if request is POST, if yes create Item object with text == item_text, redirect
-    to homepage. whether post or not, store all created Item objects in item, render 
-    home.html, with {% items %} python object in for loop being replaced the list of
-    objects in var items
-    '''
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
+    return render(request, 'home.html')
 
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
+    
