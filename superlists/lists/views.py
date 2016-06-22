71b8c6e0c3d6a4f4417f5de81133cabe3721fbda
machiner123 <igor.atakhanov@gmail.com>
2016-06-22 12:26:53 -0400

    url /new/ maps to new_list in views, form action attribute updated in templates, redunant tests deleted

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import redirect, render
-#from django.http import HttpResponse
 from lists.models import Item
 
 
@@ -14,4 +13,8 @@ def home_page(request):
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
     
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
