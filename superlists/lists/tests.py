e72529306ca591c5957ba57e53fc1fdd29c27125
machiner123 <igor.atakhanov@gmail.com>
2016-06-22 20:45:03 -0400

    figuring out seperate lists, prog ='http://chimera.labs.oreilly.com/books/1234000000754/ch06.html#_capturing_parameters_from_urls'

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -61,24 +61,33 @@ class ListAndItemModelTest(TestCase):
 class ListViewTest(TestCase):
         
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        '''
+        list_ being an entry in a db, it has an id or a private key, and here we make sure
+        views rendered the right url and the right template
+        '''
+        response = self.client.get('/lists/%d/' % (list_.id))
         self.assertTemplateUsed(response, 'list.html')
 
 
-    def test_displays_all_items(self):
+    def test_displays_only_items_for_that_list(self):
         '''
-        the view here is tested by creating objects with certain attributes,
-        then checking if views rendered the right html at lists/the-only... etc.
+        every time a list object is created, we render a new response. this means that
+        this second time we create a list object, in other_list, the response must not
+        contain it
         '''
-        list_ = List.objects.create()
-        Item.objects.create(text='itemey 1', list=list_)
-        Item.objects.create(text='itemey 2', list=list_)
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other list item 1', list=other_list)
+        Item.objects.create(text='other list item 2', list=other_list)
 
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
-
+        self.assertNotContains(response, 'other list item 1')
+        self.assertNotContains(response, 'other list item 2')
 
 
 
